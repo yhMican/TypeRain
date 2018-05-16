75446880f062c0148bd15df70b622a5dcf2b4271
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-05-15 21:04:11 -0700

    rain instantiation created

---
+++ Type Rain/Assets/Scripts/Cloud.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cloud : MonoBehaviour {
+
+	private string[] test = {"テスト", "こんにちは", "菓子"};
+	private string[] testAlpha = { "tesuto", "konnnitiha", "kasi" };
+	private Text UIJ;
+	private Text UIR;
+
+	public GameObject raindrop;
+	public float RainsPerSeconds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float probability = Time.deltaTime * RainsPerSeconds;
+		if (Random.value < probability) {
+			Rain ();
+		}
+	}
+
+	void Rain () {
+		GameObject rainDrop = Instantiate (raindrop, transform.position, Quaternion.identity);
+		rainDrop.GetComponent<Rigidbody2D> ().velocity = new Vector3 (0, -1.0f, 0);
+	}
+}
