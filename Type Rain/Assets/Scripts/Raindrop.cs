29f837970e2690d880873e1c5336bb2404fcbaa8
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-06-07 16:35:23 +0900

    180607

--- Type Rain/Assets/Scripts/Raindrop.cs
+++ Type Rain/Assets/Scripts/Raindrop.cs
@@ -4,30 +4,58 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Raindrop : MonoBehaviour {
+	static public int count = 0;
 
 	private string questionJ; // stores a question in Japanese
 	private string questionE; // stores a question in English
-	private Text textJ;
-	private Text textE;
+	private Text textJ; // stores Text Component in Japanese from a raindrop object
+	private Text textE; // stores Text Component in English from a raindrop object
+	private int index; // stores position of alphabet which a player is typing
+	private bool currentTarget; // true if a raindrop object is a target
 
 
-	void Start() {
+	// Execution order: Awake() > SetQuestion() > Start(), for some reason..
+	void Awake() {
 		textJ = transform.Find ("QuestionJ").GetComponent<Text> ();
 		textE = transform.Find ("QuestionE").GetComponent<Text> ();
+		index = 0;
+		currentTarget = false;
 	}
 	// Update is called once per frame
 	void Update () {
-		
+		//　キーを押しているかどうか
+		if (currentTarget
+			&& Input.anyKeyDown
+			&& (!Input.GetMouseButton (0) && !Input.GetMouseButton (1) && !Input.GetMouseButton (2))) {
+			//　今見ている文字とキーボードから打った文字が同じかどうか
+			if (Input.GetKeyDown (questionE [index].ToString ())) {
+				//　正解時の処理を呼び出す
+				Debug.Log ("Correct");
+				Correct ();
+			} else {
+				//　失敗時の処理を呼び出す
+				Wrong ();
+			}
+		}
 	}
-
+	private void Correct(){
+		questionE = questionE.Substring (++index);
+		if (questionE == "") {
+			Solved ();
+		} else {
+			SetQuestion (questionJ, questionE);
+		}
+	}
+	private void Wrong() {
+		Debug.Log ("Wrong");
+	}
 	public void Solved () {
 		Destroy (gameObject);
 	}
-
-	public void setQuestion (string j, string e) {
+	public void SetQuestion (string j, string e) {
 		questionJ = j;
-		questionE = e;
 		textJ.text = questionJ;
+		questionE = e;
 		textE.text = questionE;
 	}
 }
